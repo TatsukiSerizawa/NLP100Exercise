53996de7c81edfec95a8975afd139b720ff81ba7
TatsukiSerizawa <iputatsuki@gmail.com>
2020-04-17 13:33:21 +0900

    1st commit

---
+++ 01.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+moji = "パタトクカシーー"
+
+print(moji[::2])
---
+++ 02.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+pato = "パトカー"
+taku = "タクシー"
+
+for i in range(len(pato)):
+    print(pato[i], end="")
+    print(taku[i], end="")
+print("")
