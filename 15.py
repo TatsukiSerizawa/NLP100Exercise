d2daf110fc712e8945208fe224fdd538f00cfd34
TatsukiSerizawa <iputatsuki@gmail.com>
2020-04-21 13:49:09 +0900

    update

---
+++ 15.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sys
+
+df = pd.read_table('data/popular-names.txt', header=None)
+
+if len(sys.argv) < 2:
+    print("Please input a number")
+    sys.exit()
+else:
+    N = int(sys.argv[1])
+    print(df.tail(N))
